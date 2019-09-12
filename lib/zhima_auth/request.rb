4564291237644068ac128563f3a6fb041f1061fb
yc <yc@aihehuo.com>
2019-09-12 08:40:48 +0800

    switch to new api

--- lib/zhima_auth/request.rb
+++ lib/zhima_auth/request.rb
@@ -30,34 +30,35 @@ module ZhimaAuth
       @cert_name = biz_params[:cert_name]
       @cert_no = biz_params[:cert_no]
       @transaction_id = biz_params[:transaction_id]
+      @return_url = biz_params[:return_url]
     end
 
     def execute
       @response ||= RestClient.post url_with_params, {}
     end
 
-    def get_biz_no
+    def get_certify_id
       res = JSON.parse(execute)
       Validation.check_initialize_response res
-      res["zhima_customer_certification_initialize_response"]["biz_no"]
+      res["alipay_user_certify_open_initialize_response"]["certify_id"]
     end
 
     private
 
     def params
       @params ||= base_params.merge({
-        method: "zhima.customer.certification.initialize",
+        method: "alipay.user.certify.open.initialize",
         timestamp: Time.now.strftime("%Y-%m-%d %H:%M:%S"),
         biz_content: {
-          transaction_id: @transaction_id,
-          product_code: "w1010100000000002978",
+          outer_order_no: @transaction_id,
           biz_code: ZhimaAuth.configuration.biz_code,
           identity_param: {
             identity_type: "CERT_INFO",
             cert_type: "IDENTITY_CARD",
             cert_name: @cert_name,
             cert_no: @cert_no
-          }
+          },
+          merchant_config: {"return_url": @return_url}
         }.to_json
       })
     end
@@ -74,7 +75,7 @@ module ZhimaAuth
     def initialize(biz_params)
       Validation.check_certify_params biz_params
 
-      @biz_no = biz_params[:biz_no]
+      @certify_id = biz_params[:biz_no]
       @return_url = biz_params[:return_url]
     end
 
@@ -86,11 +87,11 @@ module ZhimaAuth
 
     def params
       @params ||= base_params.merge({
-        method: "zhima.customer.certification.certify",
+        method: "alipay.user.certify.open.certify",
         return_url: @return_url,
         timestamp: Time.now.strftime("%Y-%m-%d %H:%M:%S"),
         biz_content: {
-          biz_no: @biz_no
+          certify_id: @certify_id
         }.to_json
       })
     end
@@ -114,90 +115,20 @@ module ZhimaAuth
     def get_certify_result
       result = execute
       res = JSON.parse(result)
-      Validation.check_query_response res
-      res["zhima_customer_certification_query_response"]
+      # Validation.check_query_response res
+      res["alipay_user_certify_open_query_response"]["passed"]
     end
 
     private
 
     def params
       @params ||= base_params.merge({
-        method: "zhima.customer.certification.query",
+        method: "alipay.user.certify.open.query",
         timestamp: Time.now.strftime("%Y-%m-%d %H:%M:%S"),
         biz_content: {
-          biz_no: @biz_no
-        }.to_json
-      })
-    end
-
-  end
-
-  class AuthQueryRequest < BaseRequest
-    def initialize biz_params
-      @userId = biz_params[:userId]
-    end
-
-    def execute
-      "alipays://platformapi/startapp?appId=20000067&url=" + CGI.escape(url_with_params)
-    end
-
-    private
-    def url_with_params
-      [url, WebUtil.to_query(params_with_sign)].join("?")
-    end
-
-    def params
-      @params ||= base_params.merge({
-        method: "zhima.auth.info.authquery",
-        timestamp: Time.now.strftime("%Y-%m-%d %H:%M:%S"),
-        biz_content: {
-          identity_type: "5",
-          identity_param: {
-            userId: @userId
-          },
-          auth_category: "C2ConB"
+          certify_id: @biz_no
         }.to_json
       })
     end
   end
-
-  class MutualViewApplyRequest < BaseRequest
-    def initialize biz_params
-      @cert_type = biz_params[:cert_type]
-      @cert_name = biz_params[:cert_name]
-      @cert_no = biz_params[:cert_no]  
-      @callback_url = biz_params[:callback_url]  
-      @ext_biz_param = biz_params[:ext_biz_param]   
-    end
-
-    def execute
-      "alipays://platformapi/startapp?appId=20000067&url=" + CGI.escape(url_with_params)
-    end
-
-    private
-    def url_with_params
-      [url, WebUtil.to_query(params_with_sign)].join("?")
-    end
-
-    def params
-      @params ||= base_params.merge({
-        method: "zhima.customer.auth.mutualview.apply",
-        timestamp: Time.now.strftime("%Y-%m-%d %H:%M:%S"),
-        biz_content: {
-          product_param: {
-            productCode: "w1010100001000002181"
-          },
-          biz_type: "self",
-          identity_param: {
-            certType: "IDENTITY_CARD",
-            name: @cert_name,
-            certNo: @cert_no
-          }, 
-          callback_url: @callback_url,
-          ext_biz_param: @ext_biz_param
-        }.to_json
-      })
-    end
-  end
-
 end
